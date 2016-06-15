3335e75caced5d1104b1d3cf7c454fc31539a8e3
lomlo <michael.lo@live.co.uk>
2016-06-15 12:51:42 +0100

    draft of finding links by tags

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,10 +1,22 @@
 # I would like to see a list of links on the homepage
 
 feature 'viewing links' do
-  scenario 'user visits homepage and can see links' do
+  before(:each) do
+    Link.create(url: 'www.makersacadmey.com', name: 'makers',
+    tags: [Tag.first_or_create(name: 'study')])
+    Link.create(url: 'www.yahoo.com', name: 'search engine',
+    tags: [Tag.first_or_create(name: 'bubbles')])
+    Link.create(url: 'www.github.com', name: 'save',
+    tags: [Tag.first_or_create(name: 'study')])
+    Link.create(url: 'www.heroku.com', name: 'server',
+    tags: [Tag.first_or_create(name: 'uploads')])
+  end
+
+  xscenario 'user visits homepage and can see links' do
     Link.create(url: 'www.google.com', name: 'google')
     visit '/'
     expect(page.status_code).to eq 200
     expect(page).to have_content('www.google.com')
   end
+  # I would like to filter links by tag
 end
