a7119049ae7c9fa51cf34726cca05f3ed4d8fe39
awye765 <awye765@googlemail.com>
2016-06-16 14:24:32 +0100

    Step 21 - three validation - work in progress

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -10,4 +10,10 @@ feature 'User sign up' do
     expect(current_path).to eq '/users'
     expect(page).to have_content 'Password and confirmation password do not match'
   end
+
+  scenario 'requires an email' do
+    expect { sign_up(email: nil)}.not_to change(User, :count)
+    expect(current_path).to eq '/users'
+    expect(page).to have_content "You must enter a valid email address"
+  end
 end
