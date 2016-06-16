07f06dc73d76e18646d93a78ef037b89cecd41ea
lomlo <michael.lo@live.co.uk>
2016-06-16 16:34:48 +0100

    integrated error messages to be displayed to user on user sign up page

--- models/user.rb
+++ models/user.rb
@@ -12,7 +12,7 @@ class User
   # validates_format_of :email, as: :email_address
 
   property :id, Serial
-  property :email, String, format: :email_address, required: true
+  property :email, String, format: :email_address, required: true, unique: true
   property :password_digest, String, length: 60
 
   def password=(password)
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -8,14 +8,23 @@ feature 'User sign up' do
   scenario 'requires a matching confirmation password' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq '/users'
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   scenario 'requires an email' do
     expect { sign_up(email: nil)}.not_to change(User, :count)
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Email must not be blank'
   end
 
   scenario 'requires a valid email input' do
     expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Email has an invalid format'
+  end
+
+  scenario 'I cannot sign up with an existing email' do
+    expect { sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 end
